9be5166902908e9a0be0f125bb85084bdc33d5c3
Alex Lee <alexlee@Alexs-Air.lan>
2026-06-27 15:23:25 -0400

    added pages' '

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
 
 export default function LandingPage() {
-  
+
+  return (
+    <main className="bg-zinc-100 min-h-screen">
+      <div className="p-6"> 
+        {/* Hero Section */}
+        <section className="bg-slate-600">
+          <div>
+
+          </div>
+        </section>
+      </div>
+    </main>
+  )
 }
